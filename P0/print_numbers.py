4ce982fe0d216cfbc2bdbe86c6dbbec6affaf818
obijuan <juan@iearobotics.com>
2019-01-27 18:31:26 +0100

    final print added

--- P0/print_numbers.py
+++ P0/print_numbers.py
@@ -1,6 +1,8 @@
 print("printing the numbers from 1 to 20. One number per line")
 
-#-- Initial number: 1
-#-- Stop number: 21 (It is not shown)
+# Initial number: 1
+# Stop number: 21 (It is not shown)
 for i in range(1, 21):
     print(i)
+
+print("The program is finished")
