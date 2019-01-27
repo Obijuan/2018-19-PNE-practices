8179d0abf80ebcb8497a1bd1b956263608df5ff3
obijuan <juan@iearobotics.com>
2019-01-27 18:08:51 +0100

    hello.py stored in the P0 folder

---
+++ P0/hello.py
@@ -0,0 +1,9 @@
+# my first python program in the pycharm ide
+
+print("Practice 0: Hello world!")
+print("Hello world!")
+print("Testing..... Line 2")
+print("Testing..... line 3")
+print("This is the end")
+print("This is another line.....")
+print("Another line for testing... added!!")
