066455aad9bd108aac74296464efbea7a7879173
obijuan <juan@iearobotics.com>
2019-01-27 18:41:06 +0100

    sum_100.py added

---
+++ P0/sum_100.py
@@ -0,0 +1,8 @@
+print("Adding the numbers from 1 to 100, and printing the result")
+
+# Variable for storing the total sum of the numbers
+total_sum = 0
+for i in range(1, 101):
+    total_sum += i
+
+print("The total sum of the first 100 numbers is: {}".format(total_sum))
